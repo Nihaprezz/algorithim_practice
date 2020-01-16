804e32e46390ea1b02e3020780624fa3adf4d81b
nihaprezz <serg13prez@outlook.com>
2020-01-15 23:35:05 -0500

    done with sockMerchant problem

--- hacker_rank/sockMerchant.js
+++ hacker_rank/sockMerchant.js
@@ -11,15 +11,26 @@ let size = 9
 let array = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 
 function sockMerchant(n, ar) {
-    let pairs = {}
+    let pairs = {};  //object that will be manipulated to keep track of the amount of 'colors'/'numbers' in the array
+    let pairsCounter = 0; //counter that will be added to if we find a pair.
+  
     ar.forEach(num => {
-      if (pairs[num]){
-        pairs[num] += 1
+      if (pairs[num]){  //checking to see if the num is a key in the array
+        pairs[num] += 1;        //if num is a key in the object we will add 1 to it.
+  
+        if (pairs[num] == 2){   //checking the object to see if its at 2. If it is then we technicall have a pair.
+          pairs[num] = 0            //resetting the object set back to 0
+          pairsCounter++            // adding 1 to the pairs counter, since a pair was found.
+        }
+  
       } else {
-        pairs[num] = 1
+        pairs[num] = 1          //creating a object set if the num is not a key already.
       }
     })
   
-    console.log(pairs)
+    return pairsCounter 
 }
 
+sockMerchant(size, array)
+
+//DID NOT USE the size parameter so I might have done it wrong but it works... 
